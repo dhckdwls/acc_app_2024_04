43a06d22b404716bbdc78d694a957d8e0b8d6680
dhckdwls <codetama51@gmail.com>
2024-05-09 13:47:29 +0900

    주문 품목당 정산금액 산정 SQL

--- db/init.sql
+++ db/init.sql
@@ -9,6 +9,8 @@ DROP DATABASE IF EXISTS acc_app_2024_04__dev;
 CREATE DATABASE acc_app_2024_04__dev;
 USE acc_app_2024_04__dev;
 
+SHOW TABLES
+
 SELECT * FROM `member`
 
 SELECT * FROM song
@@ -21,5 +23,17 @@ SELECT * FROM cash_log
 
 SELECT * FROM order_item
 
+SELECT * FROM product_order
+
+SELECT * FROM `order`
+
+SELECT * FROM rebate_order_item
 
 
+SELECT order_item_id, pay_price, refund_price, wholesale_price, pg_fee,
+       CASE
+           WHEN pay_price = refund_price
+               THEN 0
+           ELSE wholesale_price - pg_fee
+           END AS rebate_price
+FROM `rebate_order_item`;
